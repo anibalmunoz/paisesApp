4d4a1b13928975ec0516122aa1a8c47dfcabd455
anibalmunoz <munoz4hernandez@gmail.com>
2023-02-13 15:16:39 -0600

    finalización de aplicación de paises

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -5,15 +5,25 @@ import { Country } from '../../intefaces/pais-interface';
 @Component({
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
+  styles: [
+    `
+      li {
+        cursor: pointer;
+      }
+    `,
+  ],
 })
 export class PorPaisComponent {
   termino: string = '';
   hayError: boolean = false;
   paises: Country[] = [];
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencias: boolean = false;
 
   constructor(private paisService: PaisService) {}
 
   buscar(termino: string) {
+    this.mostrarSugerencias = false;
     this.hayError = false;
     this.termino = termino;
     console.log(this.termino);
@@ -35,5 +45,16 @@ export class PorPaisComponent {
 
   sugerencias(termino: string) {
     this.hayError = false;
+    this.termino = termino;
+    this.mostrarSugerencias = true;
+    this.paisService.buscarPais(termino).subscribe(
+      (paises) => (this.paisesSugeridos = paises.splice(0, 5)),
+      (err) => (this.paisesSugeridos = [])
+    );
+  }
+
+  buscarSugerido(termino: string) {
+    this.buscar(termino);
+    this.mostrarSugerencias = false;
   }
 }
